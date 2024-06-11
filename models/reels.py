916cbf1527228ee1c36eb3ae361f89642b2481c5
César Esaú Flores Martínez <esauflores2022@gmail.com>
2024-06-11 23:45:56 +0700

    Refactor:  Modified reel structure

--- models/reels.py
+++ models/reels.py
@@ -7,9 +7,10 @@ class Reels(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(255), unique=True, nullable=False)
     caption = db.Column(db.Text, nullable=True)
-    hashtags = db.Column(JSON, nullable=True)
+    video_url = db.Column(db.String(255), nullable=True)
     thumbnail_url = db.Column(db.String(255), nullable=True)
-    saved_path = db.Column(db.String(255), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     def __repr__(self):
         return f"<Reel {self.url}>"
--- routes/reels.py
+++ routes/reels.py
@@ -1,25 +1,33 @@
 import logging
 
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint, redirect, render_template, request, url_for, session
 
-from helpers import reels
+from helpers import reels, navbar
 from models import Reels, db
+
+from flask_login import current_user, login_required
+
 
 reels_bp = Blueprint("reels", __name__)
 
 
 @reels_bp.route("/save_reel", methods=["POST"])
+@login_required
 def save_reel():
     try:
         reel_url = request.form.get("url")
-        print("reel_url", reel_url)
-        reel_id, caption, hashtags, thumbnail_url = reels.save_reel_by_url(reel_url)
+
+        # validate reel url
+        if not reels.validate_reel_url(reel_url):
+            return "Invalid reel URL"
+
+        caption, video_url, thumbnail_url = reels.get_reel_by_url(reel_url)
         new_reel = Reels(
             url=reel_url,
-            saved_path=reel_id,
             caption=caption,
-            hashtags=hashtags,
+            video_url=video_url,
             thumbnail_url=thumbnail_url,
+            user_id=current_user.id,
         )
         db.session.add(new_reel)
         db.session.commit()
@@ -27,24 +35,30 @@ def save_reel():
         logging.error("Error saving reel: %s", e)
         return f"Error saving reel: {e}"
 
-    return redirect(url_for("home.home"))
-
-
-@reels_bp.route("/reels", methods=["GET"])
-def list_reels():
-    reels = Reels.query.all()
-
-    return str(*reels)
+    return redirect(url_for("reels.show_reels"))
 
 
 @reels_bp.route("/reels/add", methods=["GET"])
+@login_required
 def add_reel():
     return render_template("reels/add_card.html", reels=reels)
 
 
 @reels_bp.route("/reels/delete/<int:id>", methods=["GET"])
+@login_required
 def delete_reel(id):
     reel = Reels.query.get(id)
     db.session.delete(reel)
     db.session.commit()
-    return redirect(url_for("home.home"))
+    return redirect(url_for("reels.show_reels"))
+
+
+@reels_bp.route("/reels", methods=["GET"])
+@login_required
+def show_reels():
+    session["navbar_menu"] = navbar.get_navmenu()
+    # reverse order of reels by current user id
+    reels = (
+        Reels.query.filter_by(user_id=current_user.id).order_by(Reels.id.desc()).all()
+    )
+    return render_template("reels/show_cards.html", reels=reels)

4321a72438bd753d2bca29edf1275e8dc42a21ff
César Esaú Flores Martínez <esauflores2022@gmail.com>
2024-06-11 23:46:34 +0700

    Feature: Added home page

--- routes/home.py
+++ routes/home.py
@@ -11,4 +11,4 @@ def home():
     session["navbar_menu"] = navbar.get_navmenu()
     # reverse order of reels
     reels = Reels.query.order_by(Reels.id.desc()).all()
-    return render_template("reels/show_cards.html", reels=reels)
+    return render_template("index.html", reels=reels)
